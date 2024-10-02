b11181ec8514ce360a358c81241255b429353bd6
rylanharper <rylanharper@gmail.com>
2024-10-02 08:46:42 -0700

    feat: updates to product and collection helper composables

--- app/composables/use-product-helpers.ts
+++ app/composables/use-product-helpers.ts
@@ -1,33 +1,37 @@
-import type { ProductFragment, MoneyFragment, ProductOptionFragment } from '@@/types/shopify'
+import type {
+  ProductFragment,
+  MoneyFragment,
+  ProductOptionFragment
+} from '@@/types/shopify';
 
 export function useProductHelpers() {
   const isNewItem = (date: string, daysOld = 30) => {
     return (
       new Date(date).valueOf() >
       new Date().setDate(new Date().getDate() - daysOld).valueOf()
-    )
-  }
+    );
+  };
 
   const isSoldOut = (product: ProductFragment) => {
-    return !product.availableForSale
-  }
+    return !product.availableForSale;
+  };
 
   const isOnSale = (price: MoneyFragment, compareAtPrice: MoneyFragment) => {
     if (compareAtPrice.amount > price.amount) {
-      return true
+      return true;
     }
-    return false
-  }
+    return false;
+  };
 
   const getColorOption = (optionInput: ProductOptionFragment[]) => {
-    const colorOptionNames = ['Color', 'Colour']
-    return optionInput.find((option) => colorOptionNames.includes(option.name)) ?? null
-  }
+    const colorOptionNames = ['Color', 'Colour'];
+    return (optionInput.find((option) => colorOptionNames.includes(option.name)) ?? null);
+  };
 
   const getSizeOption = (optionInput: ProductOptionFragment[]) => {
-    const sizeOptionNames = ['Size', 'Length']
-    return optionInput.find((option) => sizeOptionNames.includes(option.name)) ?? null
-  }
+    const sizeOptionNames = ['Size', 'Length'];
+    return (optionInput.find((option) => sizeOptionNames.includes(option.name)) ?? null);
+  };
 
   return {
     isNewItem,
@@ -35,5 +39,5 @@ export function useProductHelpers() {
     isOnSale,
     getColorOption,
     getSizeOption
-  }
+  };
 }
