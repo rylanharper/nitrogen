1f5f4bfd6be70c149cc0ecfcfe85a24355afc3ec
rylanharper <rylanharper@gmail.com>
2024-10-04 15:50:45 -0700

    chore: small grammar edits

--- app/utils/fetch-data.ts
+++ app/utils/fetch-data.ts
@@ -1,5 +1,5 @@
 /**
- * Fetches data using `useAsyncData` and the provided function and variables.
+ * Fetches graphql data using `useAsyncData` and the provided function and variables.
  * @param vars - Computed variables to pass into the fetch function
  * @param key - A unique key to ensure that data can be properly de-duplicated
  * @param fetchFn - The function used to fetch data (graphql operations)
