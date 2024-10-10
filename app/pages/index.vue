3040656e55aac118bf9e6dfc3987906695160c2c
rylanharper <rylanharper@gmail.com>
2024-10-10 12:32:41 -0700

    chore: remove test function on index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,19 +1,5 @@
 <template>
   <div>
-    <h1>Product Data</h1>
-    <div v-if="data">
-      <h2 class="text-2xl">{{ data?.title }}</h2>
-    </div>
-    <div v-else>Loading product data...</div>
+    <h1 class="text-4xl">Hello World!</h1>
   </div>
 </template>
-
-<script setup lang="ts">
-const shopify = useShopify();
-
-const { data, error } = await useAsyncData('product', () =>
-  shopify.product.get({ handle: 'salomon-xt-4-og-black' })
-);
-
-if (error.value) console.error('Error:', error.value);
-</script>
