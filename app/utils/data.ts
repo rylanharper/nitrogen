27ff0f6121d509875a0b70fac073e0bf63cf250c
rylanharper <rylanharper@gmail.com>
2024-10-10 12:29:56 -0700

    feat: update utils, rename files

--- app/utils/data.ts
+++ app/utils/data.ts
@@ -1,13 +1,13 @@
 /**
  * Fetches graphql data using `useAsyncData` and the provided function and variables.
+ * @param key - A unique key to ensure data is de-duplicated across requests
  * @param vars - Computed variables to pass into the fetch function
- * @param key - A unique key to ensure that data can be properly de-duplicated
  * @param fetchFn - The function used to fetch data (gql operation)
  * @returns An object containing data, error, and refresh
  * @see https://nuxt.com/docs/api/composables/use-async-data
  */
-export const fetchData = async <T>(key: string, vars: ComputedRef<any>, fetchFn: (vars: any) => Promise<T>) => {
-  const { data, error, refresh } = await useAsyncData(`${key}`, () =>
+export const fetchData = async <T, V>(key: string, vars: ComputedRef<V>, fetchFn: (vars: V) => Promise<T>) => {
+  const { data, error, refresh } = await useAsyncData(key, () =>
     fetchFn(vars.value), {
       watch: [vars]
     }
