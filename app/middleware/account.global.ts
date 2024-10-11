dc90392a394d7564f9e9fb20d9d293e4a85c3450
rylanharper <rylanharper@gmail.com>
2024-10-11 12:20:39 -0700

    feat: update middleware, convert routes to strings

--- app/middleware/account.global.ts
+++ app/middleware/account.global.ts
@@ -1,16 +1,16 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const isAuth = useAuthStore().isAuthenticated;
-  const secureRoutes = ['account', 'account-orders', 'account-addresses'];
+  const secureRoutes = ['account', 'account/orders', 'account/addresses'];
 
   if (secureRoutes.includes(to.name as string) && !isAuth) {
-    return navigateTo({ name: 'account-login' });
+    return navigateTo('/account/login');
   }
 
   if (to.name === 'account-login' && isAuth) {
-    return navigateTo({ name: 'account' });
+    return navigateTo('/account');
   }
 
   if (to.name === 'account-register' && isAuth) {
-    return navigateTo({ name: 'account' });
+    return navigateTo('/account');
   }
 });
