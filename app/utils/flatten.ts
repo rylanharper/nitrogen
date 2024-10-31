8d2a8a7bd4b1b02f22142d1d580c2f17260ade8f
rylanharper <rylanharper@gmail.com>
2024-10-30 21:04:27 -0700

    refactor: `flattenNodeConnection` util now handles both edges or direct nodes

--- app/utils/flatten.ts
+++ app/utils/flatten.ts
@@ -1,17 +1,21 @@
 /**
- * Flattens a connection object by extracting the nodes from its edges.
- * @param connection - The connection object containing edges with nodes
+ * Flattens a node connection object by extracting nodes from either edges or direct nodes.
+ * @param connection - The connection object containing edges or nodes
  * @returns An array of nodes
  */
-export const flattenNodeConnection = <T>(connection?: { edges?: { node: T }[] }): T[] => {
-  return connection?.edges?.map(({ node }) => node) ?? [];
-};
+export const flattenNodeConnection = <T>(
+  connection?: {
+    edges?: { node: T }[];
+    nodes?: T[];
+  }
+): T[] => {
+  if (connection?.edges) {
+    return connection.edges.map(({ node }) => node);
+  }
 
-/**
- * Flattens an array of items by extracting the nodes.
- * @param item - The array containing objects with nodes
- * @returns An array of nodes
- */
-export const flattenNodeItem = <T>(item?: { node: T }[]): T[] => {
-  return item?.map(({ node }) => node) ?? [];
+  if (connection?.nodes) {
+    return connection.nodes;
+  }
+
+  return [];
 };
