d4fefb40eb608af3de131d32a58d6a383ab9de40
rylanharper <rylanharper@gmail.com>
2024-11-19 15:04:17 -0800

    chore: small lint edits

--- app/utils/validators.ts
+++ app/utils/validators.ts
@@ -6,7 +6,7 @@
 export function isEmail(value: string): boolean {
   const regex = /^(?!.*[._+-]{2})(?!.*[._+-]$)[a-zA-Z0-9._+-]+(?<!\.)@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return regex.test(value);
-}
+};
 
 /**
  * Checks if a given value is an object.
@@ -16,7 +16,7 @@ export function isEmail(value: string): boolean {
 export function isObject(value: unknown): boolean {
   if (typeof value !== 'object' || value === null) return false;
   return value.constructor === Object;
-}
+};
 
 /**
  * Checks if a given value is an array.
@@ -25,4 +25,4 @@ export function isObject(value: unknown): boolean {
  */
 export function isArray(value: string[] | number[]): boolean {
   return Array.isArray(value);
-}
+};
