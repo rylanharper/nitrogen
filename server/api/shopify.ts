fb5e744d9ac2e31f76c732e01e10c76b1594e764
rylanharper <rylanharper@gmail.com>
2024-12-18 10:46:31 -0800

    refactor: remove `try/catch` from Shopify server proxy

--- server/api/shopify.ts
+++ server/api/shopify.ts
@@ -10,21 +10,13 @@ export default defineEventHandler(async (event) => {
 
   const endpoint = `${options.storefront}/api/${options.apiVersion}/graphql.json`;
 
-  try {
-    return await $fetch(endpoint, {
-      method: 'POST',
-      headers: {
-        accept: 'application/json',
-        'X-Shopify-Storefront-Access-Token': options.accessToken,
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify({ query, variables })
-    });
-  } catch (error) {
-    console.error('Shopify API Error:', error);
-    throw createError({
-      statusCode: 500,
-      statusMessage: 'Failed to communicate with the Shopify API.'
-    });
-  }
+  return await $fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'X-Shopify-Storefront-Access-Token': options.accessToken,
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({ query, variables })
+  });
 });
