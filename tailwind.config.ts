237e4e4970cd73acda1deeb037e8339090a5e7ad
rylanharper <rylanharper@gmail.com>
2024-12-19 15:57:22 -0800

    chore: removed unused `screens`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,6 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 export default <Partial<Config>>{
   theme: {
     extend: {
-      screens: {
-        '3xl': '1920px'
-      },
       fontFamily: {
         sans: ['IBM Plex Sans', ...defaultTheme.fontFamily.sans],
         mono: ['IBM Plex Mono', ...defaultTheme.fontFamily.mono]
