9f917f79f6e281ceda4e5bf7b9bf82cca31246c2
rylanharper <rylanharper@gmail.com>
2025-01-18 13:52:49 -0800

    feat: new graphql-client util with

--- server/utils/graphql-client.ts
+++ server/utils/graphql-client.ts
@@ -6,13 +6,38 @@ import { print } from 'graphql';
  * Sends a GraphQL query to the Shopify API via the API proxy.
  * @param query - The GraphQL query as a DocumentNode
  * @param variables - Optional variables for the GraphQL query
+ * @param maxRetries - Maximum number of retries for the fetch request
+ * @param waitTime - Wait time between fetch retries in milliseconds
  * @returns The response from the Shopify API
  */
-export const query = (query: DocumentNode, variables = {}) => {
+export const query = async (
+  query: DocumentNode,
+  variables = {},
+  maxRetries = 3,
+  waitTime = 1000
+) => {
   const serializedQuery = print(query);
 
-  return $fetch('/api/shopify', {
-    method: 'POST',
-    body: { query: serializedQuery, variables }
-  });
+  const fetchRequest = async (retryCount = 0): Promise<any> => {
+    try {
+      return await $fetch('/api/shopify', {
+        method: 'POST',
+        body: { query: serializedQuery, variables },
+      });
+    } catch (error) {
+      const count = retryCount + 1;
+
+      if (retryCount < maxRetries) {
+        console.warn(
+          `Retrying Storefront API fetch request... (${count}/${maxRetries})`
+        );
+        await new Promise((resolve) => setTimeout(resolve, waitTime));
+        return fetchRequest(count);
+      }
+
+      throw error;
+    }
+  };
+
+  return fetchRequest();
 };
