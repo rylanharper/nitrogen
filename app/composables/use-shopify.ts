314f09a5902515ad260d916527a8bdfc6ad3f98d
rylanharper <rylanharper@gmail.com>
2025-02-24 15:09:12 -0800

    feat: update app composables

--- app/composables/use-shopify.ts
+++ app/composables/use-shopify.ts
@@ -1,9 +1,9 @@
-import cart from '@@/server/operations/cart';
-import collection from '@@/server/operations/collection';
-import customer from '@@/server/operations/customer';
-import localization from '@@/server/operations/localization';
-import product from '@@/server/operations/product';
-import search from '@@/server/operations/search';
+import cart from '@@/server/operations/shopify/cart';
+import collection from '@@/server/operations/shopify/collection';
+import customer from '@@/server/operations/shopify/customer';
+import localization from '@@/server/operations/shopify/localization';
+import product from '@@/server/operations/shopify/product';
+import search from '@@/server/operations/shopify/search';
 
 export const useShopify = () => ({
   cart,
