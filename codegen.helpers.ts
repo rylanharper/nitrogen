946d60f1036d88b0e87f0dac884b581c02b3c368
rylanharper <rylanharper@gmail.com>
2025-05-23 20:35:11 -0700

    chore: remove `customerAccount` scalar types, improve default storefront scalars

--- codegen.helpers.ts
+++ codegen.helpers.ts
@@ -1,6 +1,6 @@
 /**
  * Meant to be used with GraphQL CodeGen to type the Storefront API's custom scalars correctly.
- * Reference for the GraphQL types: https://shopify.dev/docs/api/storefront/2024-07/scalars/HTML
+ * Reference for the GraphQL types: https://shopify.dev/docs/api/storefront/2025-01/scalars/HTML
  * Note: JSON is generated as 'unknown' by default
  */
 export const storefrontApiCustomScalars = {
@@ -8,20 +8,7 @@ export const storefrontApiCustomScalars = {
   DateTime: 'string',
   Decimal: 'string',
   HTML: 'string',
-  ISO8601DateTime: 'string',
-  UnsignedInt64: 'string',
-  URL: 'string'
-};
-
-/**
- * Meant to be used with GraphQL CodeGen to type the Customer Account API's custom scalars correctly.
- * Reference for the GraphQL types: https://shopify.dev/docs/api/customer/2024-07/scalars/HTML
- * Note: JSON is generated as 'unknown' by default
- */
-export const customerAccountApiCustomScalars = {
-  DateTime: 'string',
-  Decimal: 'string',
-  HTML: 'string',
+  ID: 'string',
   ISO8601DateTime: 'string',
   UnsignedInt64: 'string',
   URL: 'string'
