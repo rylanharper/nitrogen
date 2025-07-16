d5384a9a28f47c6d8dab1283c44f89ca1f80775a
rylanharper <rylanharper@gmail.com>
2025-07-15 17:08:32 -0700

    feat: update auth pages

--- app/pages/account/login.vue
+++ app/pages/account/login.vue
@@ -1,148 +1,148 @@
 <script setup lang="ts">
-import { isEmail } from '@/utils/validators';
+import { isEmail } from '@/utils/validators'
 
 // Stores
-const authStore = useAuthStore();
+const authStore = useAuthStore()
+const cartStore = useCartStore()
 
 // Customer obj
 const customer = reactive({
   email: '',
-  password: ''
-});
+  password: '',
+})
 
 // State
-const errorMessage = ref('');
-const isLoading = ref(false);
+const errorMessage = ref('')
+const isLoading = ref(false)
 
 // Login
-const isAuth = computed(() => authStore.isAuthenticated);
-const formCompleted = computed(() => customer.email && customer.password);
+const isAuth = computed(() => authStore.isAuthenticated)
+const formCompleted = computed(() => customer.email && customer.password)
 
 const handleLogin = async () => {
-  errorMessage.value = '';
-  isLoading.value = true;
+  errorMessage.value = ''
+  isLoading.value = true
 
   if (!formCompleted.value) {
-    errorMessage.value = 'Please fill out both email and password fields to sign in.';
-    isLoading.value = false;
-    return;
+    errorMessage.value = 'Please fill out both the email and password fields to sign in.'
+    isLoading.value = false
+    return
   }
 
   if (!isEmail(customer.email)) {
-    errorMessage.value = 'Please enter a valid email address.';
-    isLoading.value = false;
-    return;
+    errorMessage.value = 'Please enter a valid email address.'
+    isLoading.value = false
+    return
   }
 
   try {
-    await authStore.login(customer.email, customer.password);
+    await authStore.login(customer.email, customer.password)
 
     if (isAuth.value) {
-      await navigateTo('/account');
+      await cartStore.attachBuyer({ customerAccessToken: authStore.accessToken })
+      await navigateTo('/account')
     }
   } catch (error: any) {
-    errorMessage.value = `${error.message}. Please try again later.`;
+    errorMessage.value = `${error.message}. Please try again later.`
   } finally {
-    isLoading.value = false;
+    isLoading.value = false
   }
-};
+}
 
 // State
-const showPassword = ref(false);
+const showPassword = ref(false)
 
 // Actions
 const togglePassword = () => {
-  showPassword.value = !showPassword.value;
-};
+  showPassword.value = !showPassword.value
+}
 
 // SEO
 useHead({
-  title: 'Login'
-});
+  title: 'Login',
+})
 </script>
 
 <template>
-  <section class="relative flex flex-col items-center justify-center">
-    <div class="w-full px-6 my-32 md:max-w-lg">
-      <h1 class="text-xl tracking-tight leading-none text-center">
-        Login
-      </h1>
-      <form
-        class="flex flex-col my-6"
-        novalidate
-        @submit.prevent="handleLogin"
-      >
-        <div class="relative w-full mb-2.5">
-          <input
-            id="email"
-            v-model="customer.email"
-            name="email"
-            type="email"
-            placeholder="Email Address"
-            autocapitalize="off"
-            autocomplete="email"
-            autocorrect="off"
-            required
-            class="flex w-full py-2 px-3 bg-white border border-zinc-300 rounded-md appearance-none placeholder:text-zinc-400 focus:outline-1 focus:outline-black"
-          >
-        </div>
-        <div class="relative w-full mb-2.5">
-          <input
-            id="password"
-            v-model="customer.password"
-            name="password"
-            :type="showPassword ? 'text' : 'password'"
-            placeholder="Password"
-            autocapitalize="off"
-            autocomplete="off"
-            autocorrect="off"
-            minLength="{8}"
-            required
-            class="flex w-full py-2 px-3 bg-white border border-zinc-300 rounded-md appearance-none placeholder:text-zinc-400 focus:outline-1 focus:outline-black"
-          >
-          <button
-            type="button"
-            class="absolute inset-y-0 end-0 flex items-center px-3"
-            @click="togglePassword"
-          >
-            <Icon
-              name="ph:eye"
-              class="size-5 shrink-0"
-              :class="showPassword ? 'text-black' : 'text-zinc-400'"
-            />
-          </button>
-        </div>
-        <div class="flex mb-5">
-          <NuxtLink
-            :to="{ name: 'account-recover' }"
-            class="max-w-fit hover:text-gray-500"
-          >
-            Forgot Password?
-          </NuxtLink>
-        </div>
+  <section class="wrapper px-6 pt-28 mx-auto md:pt-32 md:max-w-[400px] md:px-0">
+    <h1 class="text-xl tracking-tight leading-none text-center">
+      Login
+    </h1>
+    <form
+      class="flex flex-col my-8"
+      novalidate
+      @submit.prevent="handleLogin"
+    >
+      <div class="relative w-full mb-2.5">
+        <input
+          id="email"
+          v-model="customer.email"
+          name="email"
+          type="email"
+          placeholder="Email Address"
+          autocapitalize="off"
+          autocomplete="email"
+          autocorrect="off"
+          required
+          class="flex w-full py-2 px-3 bg-white border border-zinc-300 rounded-md appearance-none placeholder:text-zinc-400 focus:outline-1 focus:outline-black"
+        >
+      </div>
+      <div class="relative w-full mb-2.5">
+        <input
+          id="password"
+          v-model="customer.password"
+          name="password"
+          :type="showPassword ? 'text' : 'password'"
+          placeholder="Password"
+          autocapitalize="off"
+          autocomplete="off"
+          autocorrect="off"
+          minLength="{8}"
+          required
+          class="flex w-full py-2 px-3 bg-white border border-zinc-300 rounded-md appearance-none placeholder:text-zinc-400 focus:outline-1 focus:outline-black"
+        >
         <button
-          type="submit"
-          :disabled="isLoading"
-          class="flex items-center justify-center p-2 px-4 text-normalize bg-zinc-100 border border-zinc-300 rounded-md transition duration-200 ease-in-out hover:bg-zinc-200"
+          type="button"
+          class="absolute inset-y-0 end-0 flex items-center px-3"
+          @click="togglePassword"
         >
-          {{ isLoading === true ? 'Working...' : 'Sign in' }}
+          <Icon
+            name="ph:eye"
+            class="inline-block shrink-0 !size-5"
+            :class="showPassword ? 'text-black' : 'text-zinc-400'"
+          />
         </button>
-      </form>
-      <div class="flex flex-col mb-10">
-        <p>New to Nitrogen?</p>
+      </div>
+      <div class="flex mb-5">
         <NuxtLink
-          to="/account/register"
-          class="max-w-fit underline decoration-dotted decoration-1 underline-offset-[3px] hover:text-gray-500"
+          :to="{ name: 'account-recover' }"
+          class="max-w-fit hover:text-gray-500"
         >
-          Create an account
+          Forgot Password?
         </NuxtLink>
       </div>
-      <p
-        v-if="errorMessage"
-        class="w-[75%] mx-auto text-red-500 text-center"
+      <button
+        type="submit"
+        :disabled="isLoading"
+        class="flex items-center justify-center p-2 px-4 text-normalize bg-zinc-100 border border-zinc-300 rounded-md transition duration-200 hover:bg-zinc-200"
       >
-        {{ errorMessage }}
-      </p>
+        <span>{{ isLoading === true ? 'Working...' : 'Sign in' }}</span>
+      </button>
+    </form>
+    <div class="flex flex-col mb-10">
+      <p>New to Nitrogen?</p>
+      <NuxtLink
+        to="/account/register"
+        class="max-w-fit underline decoration-dotted decoration-1 underline-offset-[3px] hover:text-gray-500"
+      >
+        <span>Create an account</span>
+      </NuxtLink>
     </div>
+    <p
+      v-if="errorMessage"
+      class="w-[75%] mx-auto text-red-500 text-center"
+    >
+      {{ errorMessage }}
+    </p>
   </section>
 </template>
--- app/pages/account/recover.vue
+++ app/pages/account/recover.vue
@@ -1,109 +1,107 @@
 <script setup lang="ts">
-import { isEmail } from '@/utils/validators';
+import { isEmail } from '@/utils/validators'
 
 // Stores
-const authStore = useAuthStore();
+const authStore = useAuthStore()
 
 // State
 const email = ref('')
-const errorMessage = ref('');
-const successMessage = ref('');
-const isLoading = ref(false);
+const errorMessage = ref('')
+const successMessage = ref('')
+const isLoading = ref(false)
 
 // Recover
-const formCompleted = computed(() => email.value);
+const formCompleted = computed(() => email.value)
 
 const handleRecover = async () => {
-  errorMessage.value = '';
-  successMessage.value = '';
-  isLoading.value = true;
+  errorMessage.value = ''
+  successMessage.value = ''
+  isLoading.value = true
 
   if (!formCompleted.value) {
-    errorMessage.value = 'Please provide a valid email address to recover your password.';
-    isLoading.value = false;
-    return;
+    errorMessage.value = 'Please provide a valid email address to recover your password.'
+    isLoading.value = false
+    return
   }
 
   if (!isEmail(email.value)) {
-    errorMessage.value = 'Please enter a valid email address.';
-    isLoading.value = false;
-    return;
+    errorMessage.value = 'Please enter a valid email address.'
+    isLoading.value = false
+    return
   }
 
   try {
-    await authStore.recover(email.value);
-    successMessage.value = 'Success! Please check your email for instructions on how to reset your password.';
+    await authStore.recover(email.value)
+    successMessage.value = 'Success! Please check your email for instructions on how to reset your password.'
   } catch (error: any) {
-    errorMessage.value = `${error.message}. Please try again later.`;
+    errorMessage.value = `${error.message}. Please try again later.`
   } finally {
-    isLoading.value = false;
+    isLoading.value = false
   }
-};
+}
 
 // SEO
 useHead({
-  title: 'Recover'
-});
+  title: 'Recover',
+})
 </script>
 
 <template>
-  <section class="relative flex flex-col items-center justify-center">
-    <div class="w-full px-6 my-32 md:max-w-lg">
-      <h1 class="text-xl tracking-tight leading-none text-center">
-        Forgot password?
-      </h1>
-      <form
-        class="flex flex-col my-6"
-        novalidate
-        @submit.prevent="handleRecover"
-      >
-        <div class="relative w-full mb-2.5">
-          <input
-            id="email"
-            v-model="email"
-            name="email"
-            type="email"
-            placeholder="Email Address"
-            autocapitalize="off"
-            autocomplete="email"
-            autocorrect="off"
-            required
-            class="flex w-full py-2 px-3 bg-white border border-zinc-300 rounded-md appearance-none placeholder:text-zinc-400 focus:outline-1 focus:outline-black"
-          >
-        </div>
-        <p class="mb-5 leading-snug text-pretty">
-          If your email is in our system, you will receive an email with
-          instructions about how to reset your password.
-        </p>
-        <button
-          type="submit"
-          :disabled="isLoading"
-          class="flex items-center justify-center p-2 px-4 text-normalize bg-zinc-100 border border-zinc-300 rounded-md transition duration-200 ease-in-out hover:bg-zinc-200"
+  <section class="wrapper px-6 pt-28 mx-auto md:pt-32 md:max-w-[400px] md:px-0">
+    <h1 class="text-xl tracking-tight leading-none text-center">
+      Forgot password?
+    </h1>
+    <form
+      class="flex flex-col my-8"
+      novalidate
+      @submit.prevent="handleRecover"
+    >
+      <div class="relative w-full mb-2.5">
+        <input
+          id="email"
+          v-model="email"
+          name="email"
+          type="email"
+          placeholder="Email Address"
+          autocapitalize="off"
+          autocomplete="email"
+          autocorrect="off"
+          required
+          class="flex w-full py-2 px-3 bg-white border border-zinc-300 rounded-md appearance-none placeholder:text-zinc-400 focus:outline-1 focus:outline-black"
         >
-          {{ isLoading === true ? 'Working...' : 'Send reset link' }}
-        </button>
-      </form>
-      <div class="flex flex-col mb-10">
-        <p>Need to login?</p>
-        <NuxtLink
-          to="/account/login"
-          class="max-w-fit underline decoration-dotted decoration-1 underline-offset-[3px] hover:text-gray-500"
-        >
-          Sign in
-        </NuxtLink>
       </div>
-      <p
-        v-if="errorMessage"
-        class="w-[75%] mx-auto text-red-500 text-center"
-      >
-        {{ errorMessage }}
+      <p class="mb-5 leading-snug text-pretty">
+        If your email is in our system, you will receive an email with
+        instructions about how to reset your password.
       </p>
-      <p
-        v-if="successMessage"
-        class="w-[75%] mx-auto text-blue-600 text-center"
+      <button
+        type="submit"
+        :disabled="isLoading"
+        class="flex items-center justify-center p-2 px-4 text-normalize bg-zinc-100 border border-zinc-300 rounded-md transition duration-200 hover:bg-zinc-200"
       >
-        {{ successMessage }}
-      </p>
+        <span>{{ isLoading === true ? 'Working...' : 'Send reset link' }}</span>
+      </button>
+    </form>
+    <div class="flex flex-col mb-10">
+      <p>Need to login?</p>
+      <NuxtLink
+        to="/account/login"
+        class="max-w-fit underline decoration-dotted decoration-1 underline-offset-[3px] hover:text-gray-500"
+      >
+        <span>Sign in</span>
+      </NuxtLink>
     </div>
+    <p
+      v-if="errorMessage"
+      class="w-[75%] mx-auto text-red-500 text-center"
+    >
+      {{ errorMessage }}
+    </p>
+    <p
+      v-if="successMessage"
+      class="w-[75%] mx-auto text-blue-600 text-center"
+    >
+      {{ successMessage }}
+    </p>
   </section>
 </template>
