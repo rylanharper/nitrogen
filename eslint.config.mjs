91cdb2c25bb6459d7c659ea10b51ea9bda7f172e
rylanharper <rylanharper@gmail.com>
2025-07-17 15:52:51 -0700

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,40 +9,21 @@ export default withNuxt(
   },
   {
     rules: {
+      'vue/no-v-html': 'off',
+      'vue/html-self-closing': 'off',
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/brace-style': ['error', '1tbs'],
+      '@stylistic/arrow-parens': ['error', 'always'],
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-dynamic-delete': 'off',
       '@typescript-eslint/no-unused-vars': [
-        'error',
+        'warn',
         {
           argsIgnorePattern: '^_',
           varsIgnorePattern: '^_',
           caughtErrorsIgnorePattern: '^_',
         },
       ],
-      'import/order': [
-        'error',
-        {
-          groups: [
-            'type',
-            ['builtin', 'external'],
-            ['internal', 'parent', 'sibling', 'index', 'object'],
-          ],
-          alphabetize: {
-            order: 'asc',
-          },
-        },
-      ],
-      'vue/no-v-html': 'off',
-      'vue/html-self-closing': 'off',
-      'vue/block-order': [
-        'error',
-        {
-          order: ['script', 'template', 'style'],
-        },
-      ],
-      '@stylistic/semi': ['error', 'never'],
-      '@stylistic/arrow-parens': ['error', 'always'],
-      '@stylistic/brace-style': ['error', '1tbs'],
     },
   },
 )
