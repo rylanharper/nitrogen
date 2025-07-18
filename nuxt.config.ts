b9709eb314135bd34c171f1a92856837de040933
rylanharper <rylanharper@gmail.com>
2025-07-17 21:20:12 -0700

    chore: remove v4 future compat option

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  future: {
-    compatibilityVersion: 4,
-  },
-
   modules: [
     '@pinia/nuxt',
     'pinia-plugin-persistedstate/nuxt',
