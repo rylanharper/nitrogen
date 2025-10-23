4b4bc1aac28f388cb3069d4705bee2207548627d
rylanharper <rylanharper@gmail.com>
2025-10-23 11:17:00 -0700

    refactor: standardize computed property naming in product card tags

--- app/components/shopify/product-card/product-card-tags.vue
+++ app/components/shopify/product-card/product-card-tags.vue
@@ -9,9 +9,9 @@ const props = defineProps<{
 }>()
 
 // Computed
-const NewItem = computed(() => isNewItem(props.product?.publishedAt))
-const SoldOut = computed(() => isSoldOut(props.product))
-const OnSale = computed(() => {
+const newItem = computed(() => isNewItem(props.product?.publishedAt))
+const soldOut = computed(() => isSoldOut(props.product.availableForSale))
+const onSale = computed(() => {
   const price = props.product?.priceRange?.minVariantPrice
   const compareAtPrice = props.product?.compareAtPriceRange?.minVariantPrice
 
@@ -26,19 +26,19 @@ const OnSale = computed(() => {
 <template>
   <div class="absolute flex gap-2 top-0 left-0 z-10 p-3 pointer-events-none">
     <span
-      v-if="NewItem"
+      v-if="newItem"
       class="px-2 py-0.5 max-w-fit uppercase text-sm bg-blue-50 text-blue-600 border border-blue-500 rounded"
     >
       New
     </span>
     <span
-      v-if="SoldOut"
+      v-if="soldOut"
       class="px-2 py-0.5 max-w-fit uppercase text-sm bg-red-50 text-red-600 border border-red-500 rounded"
     >
       Sold Out
     </span>
     <span
-      v-if="OnSale"
+      v-if="onSale"
       class="px-2 py-0.5 max-w-fit uppercase text-sm bg-yellow-50 text-yellow-600 border border-yellow-500 rounded"
     >
       Sale
