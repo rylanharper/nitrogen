09b34dcebccc23368f22a16d444c3647e322f506
rylanharper <rylanharper@gmail.com>
2026-01-06 16:56:29 -0800

    feat: update codegen config and schema

--- codegen.ts
+++ codegen.ts
@@ -3,37 +3,47 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 import { adminApiCustomScalars, storefrontApiCustomScalars } from './codegen.helpers'
 import { adminApiSchema, storefrontApiSchema } from './codegen.schema'
 
+if (!storefrontApiSchema) {
+  throw new Error(
+    'Missing required Shopify environment variables for Storefront API Codegen support.',
+  )
+}
+
 const config: CodegenConfig = {
   overwrite: true,
   generates: {
     // Admin
-    './types/shopify-admin.d.ts': {
-      schema: adminApiSchema,
-      documents: './modules/shopify/runtime/resources/graphql/admin/**/*.ts',
-      plugins: ['typescript', 'typescript-operations'],
-      config: {
-        skipTypename: true,
-        useTypeImports: true,
-        defaultScalarType: 'unknown',
-        useImplementingTypes: true,
-        enumsAsTypes: true,
-        scalars: adminApiCustomScalars,
+    ...(adminApiSchema && {
+      './types/shopify-admin.d.ts': {
+        schema: adminApiSchema,
+        documents: './modules/shopify/runtime/resources/graphql/admin/**/*.ts',
+        plugins: ['typescript', 'typescript-operations'],
+        config: {
+          skipTypename: true,
+          useTypeImports: true,
+          defaultScalarType: 'unknown',
+          useImplementingTypes: true,
+          enumsAsTypes: true,
+          scalars: adminApiCustomScalars,
+        },
       },
-    },
+    }),
     // Storefront
-    './types/shopify-storefront.d.ts': {
-      schema: storefrontApiSchema,
-      documents: './modules/shopify/runtime/resources/graphql/storefront/**/*.ts',
-      plugins: ['typescript', 'typescript-operations'],
-      config: {
-        skipTypename: true,
-        useTypeImports: true,
-        defaultScalarType: 'unknown',
-        useImplementingTypes: true,
-        enumsAsTypes: true,
-        scalars: storefrontApiCustomScalars,
+    ...(storefrontApiSchema && {
+      './types/shopify-storefront.d.ts': {
+        schema: storefrontApiSchema,
+        documents: './modules/shopify/runtime/resources/graphql/storefront/**/*.ts',
+        plugins: ['typescript', 'typescript-operations'],
+        config: {
+          skipTypename: true,
+          useTypeImports: true,
+          defaultScalarType: 'unknown',
+          useImplementingTypes: true,
+          enumsAsTypes: true,
+          scalars: storefrontApiCustomScalars,
+        },
       },
-    },
+    }),
   },
 }
 
